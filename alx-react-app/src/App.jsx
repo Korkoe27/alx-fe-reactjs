d9130f46322d8ccdc0c5c3e05878d9ef30ed56c4
Mechanic <dumashieanthony@gmail.com>
2025-07-13 04:41:30 +0000

    feat(user component): implemented props in a new component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,12 +3,14 @@ import Footer from './components/Footer';
 import MainContent from './components/MainContent';
 import Header from './components/Header';
 import './App.css'
+import UserProfile from './components/UserProfile';
 
 function App() {
 
   return (
     <div className="">
       <Header />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <WelcomeMessage />
       <MainContent />
       <Footer />
