46225c8c8f959f010b8a292e589f09b211b75460
Mechanic <dumashieanthony@gmail.com>
2025-07-13 04:28:55 +0000

    feat(content): added new components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,17 @@
-import WelcomeMessage from './components/WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage';
+import Footer from './components/Footer';
+import MainContent from './components/MainContent';
+import Header from './components/Header';
 import './App.css'
 
 function App() {
 
   return (
     <div className="">
+      <Header />
       <WelcomeMessage />
+      <MainContent />
+      <Footer />
 
     </div>
   )
