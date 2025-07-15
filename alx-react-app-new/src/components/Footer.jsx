577c2ed5c04f62231395de5d05d5c5ec64aaaf8a
Mechanic <dumashieanthony@gmail.com>
2025-07-15 23:54:10 +0000

    completed task 1 of week 10

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+
+return (
+    <footer style={{ backgroundColor:'violet',padding:'5rem',fontSize:'x-large' }}>
+        <p>© 2023 City Lovers</p>
+    </footer>
+)
+
+}
+export default Footer;
