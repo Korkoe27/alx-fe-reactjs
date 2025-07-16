269f38558037635977a9812893f437d4d7b1886d
Mechanic <dumashieanthony@gmail.com>
2025-07-16 00:00:31 +0000

    built a counter component for the task 2

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,12 +4,14 @@ import MainContent from './components/MainContent';
 import Header from './components/Header';
 import './App.css'
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
 
   return (
     <div className="">
       <Header />
+      <Counter />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <WelcomeMessage />
       <MainContent />
