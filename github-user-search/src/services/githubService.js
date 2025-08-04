600de7fe444b78319af218e55697bd042fc7ad48
Mechanic <dumashieanthony@gmail.com>
2025-08-04 00:07:34 +0000

    fixed bugs

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,15 +1,15 @@
 // services/githubService.js
 import axios from 'axios';
 
-const fetchUserData = async (username, location, minimumRepositories) => {
+const fetchUserData = async (username, location, minRepos) => {
   let query = username;
 
   if (location && location.trim()) {
     query += `+location:${location.trim()}`;
   }
 
-  if (minimumRepositories && minimumRepositories.trim()) {
-    query += `+repos:>=${minimumRepositories.trim()}`;
+  if (minRepos && minRepos.trim()) {
+    query += `+repos:>=${minRepos.trim()}`;
   }
 
 
@@ -17,7 +17,6 @@ const fetchUserData = async (username, location, minimumRepositories) => {
     const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
     return response.data;
   } catch (error) {
-    console.error("Error from GitHub:", error.response?.data || error.message);
     throw new Error(error.response?.data?.message || "Something went wrong");
   }
 };
