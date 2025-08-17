1baccf859975dd36653e99eb4f9ebbfe348eeaf2
Mechanic <dumashieanthony@gmail.com>
2025-08-17 23:31:10 +0000

    added other files

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import HomePage from './components/Homepage';
-// import RecipeDetail from './components/RecipeDetail'; // make sure this exists
+import RecipeDetail from './components/RecipeDetail'; // make sure this exists
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/recipe/:id" element={<RecipeDetail />} /> */}
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
       </Routes>
     </Router>
   )
