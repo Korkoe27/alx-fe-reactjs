9603681158bf1609a31a1c5599d98f09015c432c
Mechanic <dumashieanthony@gmail.com>
2025-08-17 23:51:21 +0000

    trying

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import HomePage from './components/Homepage';
 import RecipeDetail from './components/RecipeDetail'; // make sure this exists
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
